2f101dd43e65187a1f5752c49ba9f7c7cfc337b4
Suesi Tran <tranhathuy@gmail.com>
2021-02-23 15:39:40 +1100

    Reformat class

--- app/src/main/java/com/suesi/abcnewsfeed/retrofit/response/RssFeedsResponse.kt
+++ app/src/main/java/com/suesi/abcnewsfeed/retrofit/response/RssFeedsResponse.kt
@@ -1,9 +1,31 @@
 package com.suesi.abcnewsfeed.retrofit.response
 
-data class RssFeedsResponse(val status : String, val feed : FeedResponse, val items : List<FeedItemResponse>)
+data class RssFeedsResponse(
+    val status: String,
+    val feed: FeedResponse,
+    val items: List<FeedItemResponse>
+)
 
-data class FeedResponse(val url : String, val title : String, val link : String, val author: String, val description : String, val image : String)
+data class FeedResponse(
+    val url: String,
+    val title: String,
+    val link: String,
+    val author: String,
+    val description: String,
+    val image: String
+)
 
-data class FeedItemResponse(val title : String, val pubDate : String, val link : String, val guid : String, val author : String, val thumbnail : String, val description : String, val content : String, val enclosure : EnclosureResponse, val categories : List<String>)
+data class FeedItemResponse(
+    val title: String,
+    val pubDate: String,
+    val link: String,
+    val guid: String,
+    val author: String,
+    val thumbnail: String,
+    val description: String,
+    val content: String,
+    val enclosure: EnclosureResponse,
+    val categories: List<String>
+)
 
-data class EnclosureResponse(val link : String, val type : String, val thumbnail : String)
+data class EnclosureResponse(val link: String, val type: String, val thumbnail: String)
